fa6e5040e6b83389e985c727d31f75270d713ec8
Artemiy2001 <art.barkovskii@mail.ru>
2024-06-22 16:10:59 +0300

    second commit

---
+++ src/home_work_1/BitwiseOperation.java
@@ -0,0 +1,21 @@
+package home_work_1;
+
+import java.util.Scanner;
+
+public class BitwiseOperation {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int firstNum = in.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int secondNum = in.nextInt();
+
+        System.out.println("Вы ввели: " + firstNum + " и " + secondNum);
+
+        int result = firstNum | secondNum;
+
+        System.out.println(result);
+    }
+}
