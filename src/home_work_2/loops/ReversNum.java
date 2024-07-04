fc8a8578021e7b8330e5dabe6ececc7d24647627
Artemiy2001 <art.barkovskii@mail.ru>
2024-07-05 00:44:18 +0300

    task 1.5.4, 1.5.5, 1.5.6

--- src/home_work_2/loops/ReversNum.java
+++ src/home_work_2/loops/ReversNum.java
@@ -11,6 +11,10 @@ public class ReversNum {
         System.out.print("Введите число: ");
         int num = scanner.nextInt();
 
+        if (num < 0){
+            num *= -1;
+        }
+
         while (num > 0){
 
             System.out.print(num % 10);
